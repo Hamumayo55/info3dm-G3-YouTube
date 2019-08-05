36b5cd181e1c31081d7e2b2f9da5e510e2d72642
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2019-08-05 21:31:56 +0900

    add doctest morphogical

--- youtube_exp/morphogical.py
+++ youtube_exp/morphogical.py
@@ -3,24 +3,30 @@ from janome.tokenizer import Tokenizer
 import collections
 
 """
-形態素解析でタイトルによく出てくるワードを
-カウントするプログラム
+形態素解析でタイトルによく出てくるワードをカウントするプログラム
+morphological(df_fram)では正しく動いているかをテストしている。
+実装として使うときはdecision.pyのmorphological(df_fram)を参照
 """
 
 df = YouTuber.channel_videos.UUUM_videos()
 df_Hikakin = df[df['cid'] == 'UCZf__ehlCEBPop___sldpBUQ'].reset_index(drop=True)
 
+
 def morphological(df_fram):
 
     """
     タイトルの単語を分けてワードカウントを行う
-
-    Parameters
     ----------
-    df : pandas.core.frame.DataFrame
+    Parameters
+    df_fram : pandas.core.frame.DataFrame
+    ----------
+    Return
+    type(c)：<class 'collections.Counter'>
+    ----------
+    >>> morphological(df_Hikakin)
+    True
     """
-    
-    df_fram['len_title'] = df_fram['title'].apply(lambda x: len(str(x).replace(' ', '')))
+
     df_OneMillion = df_fram[df_fram["viewCount"] >3000000]
      
     list_tword = []
@@ -30,6 +36,11 @@ def morphological(df_fram):
         for token in t.tokenize(title):
             list_tword.append((token.surface))
             c = collections.Counter(list_tword)
-    print(c)
+
+    return issubclass(type(c), dict)
 
 morphological(df_Hikakin)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
